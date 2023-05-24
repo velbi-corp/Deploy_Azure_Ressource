6470cd966ef088252144098fe588aa3dfef9ff7c
PASE04211 <ext.antoine.specque@sncf.fr>
2023-05-24 15:07:09 +0200

    rrr

--- provider.tf
+++ provider.tf
@@ -12,7 +12,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = "dbf62859-39f7-4fdc-bb53-e526725de55e"
-  tenant_id       = "419583d9-10bf-4640-938d-efe83663b051"
-
 }
